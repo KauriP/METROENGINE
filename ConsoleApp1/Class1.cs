2f5cc0f105f1337fed4e5f68ccbe5567d1ad5c42
Sandevee <santeri.vakkilainen@hotmail.com>
2018-10-25 11:46:06 +0300

    Testi

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+
+    
+}
