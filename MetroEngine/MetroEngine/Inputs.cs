35236ee83cd1e9ace8580782d098f6a655fa1865
Sandevee <santeri.vakkilainen@hotmail.com>
2018-11-01 11:45:13 +0200

    .

--- MetroEngine/MetroEngine/Inputs.cs
+++ MetroEngine/MetroEngine/Inputs.cs
@@ -2,7 +2,12 @@
 
 public class Inputs
 {
-	public Inputs()
-	{
-	}
+	public KeyCode jump { get; set; }
+	public KeyCode left { get; set; }
+	public KeyCode right { get; set; }
+    public KeyCode forward { get; set; }
+    public KeyCode backward { get; set; }
+    public KeyCode extra1 { get; set; }
+    public KeyCode extra2 { get; set; }
+
 }
