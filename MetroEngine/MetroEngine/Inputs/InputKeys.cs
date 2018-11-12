e5f5640fa35f4bb0bad9a468df620f311a6a0dfb
Sandevee <santeri.vakkilainen@hotmail.com>
2018-11-12 09:49:34 +0200

    .

---
+++ MetroEngine/MetroEngine/Inputs/InputKeys.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetroEngine
+{
+    //public class InputKeys
+    //{ 
+  
+	   // public Keys jump { get; set; }
+	   // public KeyCode left { get; set; }
+	   // public KeyCode right { get; set; }
+    //    public KeyCode forward { get; set; }
+    //    public KeyCode backward { get; set; }
+    //    public KeyCode action1 { get; set; }
+    //    public KeyCode action2 { get; set; }
+    //    public KeyCode action3 { get; set; }
+    //    public KeyCode action4 { get; set; }
+    //    public KeyCode action5 { get; set; }
+
+        
+        
+
+
+    //}*/
+}
